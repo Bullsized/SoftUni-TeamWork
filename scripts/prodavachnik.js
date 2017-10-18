ff90b0967a7741893c46e3bbaa78ef117b61f476
Bullsized <ivanstefanovbg@gmail.com>
2017-10-18 23:19:44 +0300

    little help for 7.2
    fixed the .js file

--- scripts/prodavachnik.js
+++ scripts/prodavachnik.js
@@ -201,7 +201,8 @@ function startApp() {
             method: "GET",
             url: kinveyUserUrl,
             headers: kinveyAuthHeaders,
-            success: afterPublisherRequest
+            success: afterPublisherRequest,
+            error: showError
         });
 
         function afterPublisherRequest(publisher) {
@@ -218,11 +219,13 @@ function startApp() {
                 url: kinveyAdvertsUrl,
                 headers: kinveyAuthHeaders,
                 data: advertData,
-                success: createAdvertSuccess
+                success: createAdvertSuccess,
+                error: handleAjaxError
             });
 
             function createAdvertSuccess(response) {
                 listAdverts();
+				showInfo('Advertisement created.');
             }
         }
     }
@@ -239,11 +242,13 @@ function startApp() {
             method: "DELETE",
             url: kinveyBookUrl,
             headers: kinveyAuthHeaders,
-            success: deleteBookSuccess
+            success: deleteBookSuccess,
+            error: handleAjaxError
         });
 
         function deleteBookSuccess(response) {
             listAdverts();
+			showInfo('Advert deleted.');
         }
     }
 
@@ -259,7 +264,8 @@ function startApp() {
             method: "GET",
             url: kinveyBookUrl,
             headers: kinveyAuthHeaders,
-            success: loadAdvertForEditSuccess
+            success: loadAdvertForEditSuccess,
+            error: handleAjaxError
         });
 
         function loadAdvertForEditSuccess(advert) {
@@ -292,11 +298,13 @@ function startApp() {
             url: kinveyAdvertUrl,
             headers: kinveyAuthHeaders,
             data: advertData,
-            success: editAdvertSuccess
+            success: editAdvertSuccess,
+            error: handleAjaxError
         });
 
         function editAdvertSuccess(response) {
             listAdverts();
+            showInfo('Advertisement edited.');
         }
     }
 
